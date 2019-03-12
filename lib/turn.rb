dd6773897cb2b1aaab14ea38d3104bf5b984ee7d
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-03-12 15:03:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index) == true 
-    move(board, index, position)
+    move(board, index)
   else
     puts "try again"
   end
