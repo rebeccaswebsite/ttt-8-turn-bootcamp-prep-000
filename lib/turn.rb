d2042c7cc86a8231cd751750c10b08e3d3f8337f
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-03-12 15:02:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move? == true 
+  if valid_move?(board, index) == true 
     move(board, index, position)
   else
     puts "try again"
