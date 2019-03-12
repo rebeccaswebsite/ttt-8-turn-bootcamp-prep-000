54695a3e2e946ce9728b82f93530f5c42b95cff2
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-03-12 14:57:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,7 +16,7 @@ def position_taken?(board,index)
 end 
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?
+  if index.between?(0,8) && !position_taken?(board,index)
     return true
   else
     return false
